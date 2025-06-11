3f1434bbe220aaa6d732ed7714f7ce4d6e9b4a2f
KTakahiro1729 <poet.takahiro1234@gmail.com>
2025-06-11 18:05:57 +0900

    Add base UI components

---
+++ src/components/common/BaseListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="base-list-item">
+    <div v-if="showDeleteButton" class="delete-button-wrapper">
+      <button
+        type="button"
+        class="button-base list-button list-button--delete"
+        @click="emitDelete"
+        :disabled="!canDelete"
+        aria-label="項目を削除"
+      >
+        －
+      </button>
+    </div>
+    <slot />
+  </li>
+</template>
+
+<script setup>
+defineProps({
+  showDeleteButton: Boolean,
+  canDelete: { type: Boolean, default: true },
+});
+const emit = defineEmits(['delete-item']);
+function emitDelete() {
+  emit('delete-item');
+}
+</script>
+
