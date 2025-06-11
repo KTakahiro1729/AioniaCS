3343021b6bc1a06360e343ab4b8530d15f8764be
KTakahiro1729 <poet.takahiro1234@gmail.com>
2025-06-11 19:32:02 +0900

    refactor: split footer and drive UI

---
+++ src/components/ui/TopLeftControls.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="top-left-controls">
+    <div class="google-drive-button-container">
+      <button
+        class="button-base icon-button"
+        title="Google Drive Menu"
+        v-if="isGapiInitialized && isGisInitialized"
+        @click="toggleMenu"
+        ref="driveMenuToggleButton"
+      >
+        <span class="icon-svg icon-svg-cloud" aria-label="Google Drive"></span>
+      </button>
+      <div class="floating-menu" v-if="showMenu" ref="driveMenu">
+        <div class="menu-item status-message" id="floating_drive_status_message">
+          {{ driveStatusMessage }}
+        </div>
+        <button
+          class="menu-item button-base"
+          v-if="canSignInToGoogle"
+          @click="handleSignIn"
+        >
+          Sign In with Google
+        </button>
+        <button
+          class="menu-item button-base"
+          v-if="isSignedIn"
+          @click="handleSignOut"
+        >
+          Sign Out
+        </button>
+        <button
+          class="menu-item button-base"
+          v-if="isSignedIn"
+          @click="handleChooseFolder"
+          :disabled="!isSignedIn"
+        >
+          Choose Drive Folder
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch, nextTick } from 'vue';
+
+const props = defineProps({
+  isGapiInitialized: Boolean,
+  isGisInitialized: Boolean,
+  driveStatusMessage: String,
+  canSignInToGoogle: Boolean,
+  isSignedIn: Boolean,
+});
+
+const emit = defineEmits(['sign-in', 'sign-out', 'choose-folder']);
+
+const showMenu = ref(false);
+const driveMenuToggleButton = ref(null);
+const driveMenu = ref(null);
+let driveMenuClickListener = null;
+
+function toggleMenu() {
+  showMenu.value = !showMenu.value;
+}
+
+function handleSignIn() {
+  showMenu.value = false;
+  emit('sign-in');
+}
+
+function handleSignOut() {
+  showMenu.value = false;
+  emit('sign-out');
+}
+
+function handleChooseFolder() {
+  showMenu.value = false;
+  emit('choose-folder');
+}
+
+watch(showMenu, (newValue) => {
+  if (driveMenuClickListener) {
+    document.removeEventListener('click', driveMenuClickListener, true);
+    driveMenuClickListener = null;
+  }
+  if (newValue) {
+    nextTick(() => {
+      const menuEl = driveMenu.value;
+      const toggleButtonEl = driveMenuToggleButton.value;
+      if (menuEl && toggleButtonEl) {
+        driveMenuClickListener = (event) => {
+          if (!menuEl.contains(event.target) && !toggleButtonEl.contains(event.target)) {
+            showMenu.value = false;
+          }
+        };
+        document.addEventListener('click', driveMenuClickListener, true);
+      }
+    });
+  }
+});
+</script>
+
+<style scoped>
+</style>
+
