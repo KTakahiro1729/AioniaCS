8d47c9f8e3a32c2d1b5ba143cdbe162d17fb0d1b
KTakahiro1729 <poet.takahiro1234@gmail.com>
2025-06-22 11:06:54 +0900

    refactor keyboard handling

--- src/composables/useKeyboardHandling.js
+++ src/composables/useKeyboardHandling.js
@@ -23,38 +23,12 @@ export function useKeyboardHandling() {
     }
   }
 
-  function onFocus(event) {
-    if (event.target.matches("input, textarea")) {
-      setTimeout(handleViewportResize, 200);
-    }
-  }
-
-  function onFocusOut(event) {
-    if (event.target.matches("input, textarea")) {
-      setTimeout(() => {
-        body.classList.remove("keyboard-visible");
-        root.style.removeProperty("--keyboard-height");
-        keyboardVisible = false;
-      }, 200);
-    }
-  }
-
   onMounted(() => {
-    document.addEventListener("focusin", onFocus);
-    document.addEventListener("focusout", onFocusOut);
     window.visualViewport.addEventListener("resize", handleViewportResize);
-    if (
-      document.activeElement &&
-      (document.activeElement.matches("input") ||
-        document.activeElement.matches("textarea"))
-    ) {
-      setTimeout(handleViewportResize, 200);
-    }
+    handleViewportResize();
   });
 
   onBeforeUnmount(() => {
-    document.removeEventListener("focusin", onFocus);
-    document.removeEventListener("focusout", onFocusOut);
     window.visualViewport.removeEventListener("resize", handleViewportResize);
     body.classList.remove("keyboard-visible");
     root.style.removeProperty("--keyboard-height");
